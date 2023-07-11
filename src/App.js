ffa828e2d69510b82e0eac7852e15ad70589ed27
Dhruv Arora <dhruvarora.code@gmail.com>
2023-07-11 20:40:34 +0530

    implementing date timer

---
+++ src/App.js
@@ -0,0 +1,19 @@
+export default function App() {
+  return (
+    <>
+      <div style={{ display: "flex" }}>
+        <button>-</button>
+        <h3>Step: 5</h3>
+        <button>+</button>
+      </div>
+
+      <div style={{ display: "flex" }}>
+        <button>-</button>
+        <h3>Count: 5</h3>
+        <button>+</button>
+      </div>
+
+      <p>30 days from today is Wed Jul 21 2027</p>
+    </>
+  );
+}
