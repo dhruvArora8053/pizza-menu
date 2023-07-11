86328bba37d452c399b65b5a5c5aa1238a229c19
Dhruv Arora <dhruvarora.code@gmail.com>
2023-07-11 20:55:13 +0530

    implementing step and count

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
+import { useState } from "react";
+
 export default function App() {
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+
+  function handleStepPrevious() {
+    if (step > 1) setStep((s) => s - 1);
+  }
+
+  function handleStepNext() {
+    setStep((s) => s + 1);
+  }
+
+  function handleCountPrevious() {
+    setCount((c) => step - c);
+  }
+
+  function handleCountNext() {
+    setCount((c) => step + c);
+  }
+
   return (
     <>
       <div style={{ display: "flex" }}>
         <button>-</button>
-        <h3>Step: 5</h3>
+        <h3>Step: {step}</h3>
         <button>+</button>
       </div>
 
       <div style={{ display: "flex" }}>
         <button>-</button>
-        <h3>Count: 5</h3>
+        <h3>Count: {count}</h3>
         <button>+</button>
       </div>
 
