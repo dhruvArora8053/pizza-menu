1331dbbfe229f8bd8b3cc21ee2b7ae3d28287c65
Dhruv Arora <dhruvarora.code@gmail.com>
2023-07-13 21:19:22 +0530

    challenge implemented

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import { useState } from "react";
 
 export default function App() {
-  const [step, setStep] = useState(1);
+  const [step, setStep] = useState(0);
   const [count, setCount] = useState(0);
 
-  function handleStepPrevious() {
-    if (step > 1) setStep((s) => s - 1);
-  }
+  // function handleStepPrevious() {
+  //   if (step > 1) setStep((s) => s - 1);
+  // }
 
-  function handleStepNext() {
-    setStep((s) => s + 1);
-  }
+  // function handleStepNext() {
+  //   setStep((s) => s + 1);
+  // }
 
   function handleCountPrevious() {
     setCount((c) => c - step);
   }
 
   function handleCountNext() {
-    setCount((c) => step + c);
+    setCount((c) => c + step);
   }
 
   const now = new Date();
@@ -27,11 +27,27 @@ export default function App() {
   const year = now.getFullYear();
 
   return (
-    <>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
       <div style={{ display: "flex" }}>
-        <button onClick={handleStepPrevious}>-</button>
-        <h3>Step: {step}</h3>
-        <button onClick={handleStepNext}>+</button>
+        <h3>Step:</h3>
+        <input
+          value={step}
+          onChange={(e) => {
+            setStep(Math.abs(e.target.value));
+            console.log(e.target.value);
+          }}
+          type="range"
+          min="0"
+          max="10"
+        />
+        <div>{step}</div>
       </div>
 
       <div style={{ display: "flex" }}>
@@ -45,6 +61,6 @@ export default function App() {
       ) : (
         <p>{Math.abs(count)} days ago was Tue Jun 15 2027</p>
       )}
-    </>
+    </div>
   );
 }
